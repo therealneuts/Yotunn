a931c7a05f7be492ddc8ae9575252358310a0517
AWhiteSheep <43411943+AWhiteSheep@users.noreply.github.com>
2018-11-29 17:02:17 -0500

    updating the UI to see shards amount

--- Yotunn/Assets/Scripts/UI/ReserveMana.cs
+++ Yotunn/Assets/Scripts/UI/ReserveMana.cs
@@ -8,7 +8,9 @@ public class ReserveMana : MonoBehaviour {
 
 
     [Header("Référenciez le nombre de mana default")]
-    public int DefaultNombrePourTour;    
+    public int DefaultNombrePourTour;
+    [Header("Pour les tests seulements")]
+    public int inTestShard;
     [Header("Référenciez toutes les images des shards")]
     //List avec les images des mana shards des instances de la classe UI
     public List<Image> lsImageShards;
@@ -32,23 +34,74 @@ public class ReserveMana : MonoBehaviour {
             else if (value < 0)
                 _inNombrePourTour = 0;
             else
+                //si la valeur n'est pas plus grande que 10 et pas plus petite que 0
                 _inNombrePourTour = value;
+
         }
     }
 
-    //Besoin d'un int du nombre totale de mana libre ou non UTILISÉ
-    private int _inNombreShardDispo;
+    //Besoin d'un int du nombre totale de mana libre ou NON UTILISÉ
+    private int _inNombreShardNonUtilise;
     public int NombreShardDispo
     {
-        get { return _inNombreShardDispo; }
+        get { return _inNombreShardNonUtilise; }
         set
         {
-            //Si le nombre _inNombreShardDispo est plus petit que 1 de setter à zero
-            if (_inNombreShardDispo < 1)
-                _inNombreShardDispo = 0;
+            //Si le nombre _inNombreShardNonUtilise est plus petit que 1 de setter à zero
+            //afin de ne pas a avoir à gérer avec les exceptions
+            if (value < 1)
+            {
+                //Zéro
+                _inNombreShardNonUtilise = 0;
+                //FAire certain que toutes les images affichés
+                //Son d'une transparence gray
+                for (int i = 0; i < _inNombrePourTour; i++)
+                {
+                    lsImageShards[i].color = Color.gray;
+                }
+            }
+            //Si la valeur est plus grande que le nombre que le tour permet
+            //Setter la valeur pour le nombre que le tour permet
+            else if (value > _inNombrePourTour)
+            {
+                _inNombreShardNonUtilise = _inNombrePourTour;
+
+                for (int i = 0; i < _inNombrePourTour; i++)
+                {
+                    lsImageShards[i].color = Color.white;
+                }
+            }
+            //si la valeur est plus grande que le nombre _inNombreShardNonUtilise
+            //nous ajoutons des shards on doit faire certain que les shard image soit visible
+            else if (value > _inNombreShardNonUtilise)
+            {
+                //Changer la valeur desShardsNonUtilisé pour la valeur
+                _inNombreShardNonUtilise = value;
+
+                //partant de l'index 0 j'usqu'au nombreNonUtilisé
+                for(int i = 0; i < _inNombreShardNonUtilise; i++)
+                {
+                    lsImageShards[i].color = Color.white;
+                }
+            }//Sinon si la valeur est plus petite on enlève une shard
+            else if(value < _inNombreShardNonUtilise)
+            {
+                _inNombreShardNonUtilise = value;
+
+                //Partant de l'index des shards non utilisés jusqu'au max du tour donné
+                for (int i = _inNombreShardNonUtilise; i < _inNombrePourTour; i++)
+                {
+                    lsImageShards[i].color = Color.grey;
+                }
+            }
 
             //Changer le text après avoir fait les correction
-            txtReserveShard.text = string.Format("{0}/{1}", _inNombreShardDispo, _inNombrePourTour);
+            txtReserveShard.text = string.Format("{0}/{1}", _inNombreShardNonUtilise, _inNombrePourTour);
+
+            ////trouve le nombre de ShardImage qui sont invisibles 
+            //int indexDebut = lsImageShards.FindAll(ShardImage => ShardImage.color == Color.clear).Count - 1;
+
+
         }
     }
 
@@ -63,6 +116,21 @@ public class ReserveMana : MonoBehaviour {
     {
         //Initialise les shards qui vont être vue dans le jeux
         InitialisationShards();
+
+        //Pour les tests
+        if (Application.isEditor)
+        {
+            inTestShard = DefaultNombrePourTour;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //Pour les tests
+        if (Application.isEditor)
+        {
+            NombreShardDispo = inTestShard;
+        }
     }
 
     //cette méthode sera appellé au début du jeu et à chaque tour 
@@ -71,8 +139,9 @@ public class ReserveMana : MonoBehaviour {
         //Si ma liste n'est pas null --> donc avec des images pour les images
         if (lsImageShards != null)
         {
+
             //à l'initialisation le nombreDisponible est égale au nombre donné pour le tour
-            _inNombreShardDispo = _inNombrePourTour;
+            _inNombreShardNonUtilise = _inNombrePourTour;
 
             //pour que i ne soit pas pa égal ou plus grand que la grandeur de la liste
             for (int i = 0; i < lsImageShards.Count; i++)
@@ -90,7 +159,7 @@ public class ReserveMana : MonoBehaviour {
                 lsImageShards[i].color = Color.white;
             }
 
-            txtReserveShard.text = string.Format("{0}/{1}", _inNombreShardDispo, _inNombrePourTour);
+            txtReserveShard.text = string.Format("{0}/{1}", _inNombreShardNonUtilise, _inNombrePourTour);
         }
 
     }
