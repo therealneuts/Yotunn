2330880b2ccc5c1b54346af36cc2350e8650fc4d
Roflinwaffle <43412393+Roflinwaffle@users.noreply.github.com>
2018-12-07 17:16:53 -0500

    Graveyard and Deck

---
+++ Yotunn/Assets/Scripts/GameEngine/GraveyardBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    List<CardManager> CardsinGraveyard = new List<CardManager>();
+
+    public void AddCardToGraveyard(CardManager card)
+    {
+        CardsinGraveyard.Add(card);
+    }
+
+}
