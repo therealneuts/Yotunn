04fa4a7166f8fdddd3bc5b0fca131fa38165922f
Roflinwaffle <43412393+Roflinwaffle@users.noreply.github.com>
2018-12-10 09:36:20 -0500

    Graveyard-Deck Relation

--- Yotunn/Assets/Scripts/GameEngine/GraveyardBehavior.cs
+++ Yotunn/Assets/Scripts/GameEngine/GraveyardBehavior.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour {
 
     List<CardManager> CardsinGraveyard = new List<CardManager>();
+    int GraveyardSize;
+
 
     public void AddCardToGraveyard(CardManager card)
     {
         CardsinGraveyard.Add(card);
     }
 
+    public void ShuffleinDeck()
+    {
+        DeckBehavior deck = GetComponentInParent<DeckBehavior>();
+        foreach (CardManager carte in CardsinGraveyard)
+        {
+            deck.CardsinDeck.Push(carte);
+            CardsinGraveyard.Remove(carte);
+            GraveyardSize = GraveyardSize - 1;
+        }
+    }
 }
