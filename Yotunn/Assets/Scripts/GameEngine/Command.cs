350f7c7f7f1a51dd0c6c901296c0b3c7221fd9ad
awhitesheep <yanharc@gmail.com>
2018-12-10 09:23:29 -0500

    GameSceneBattleGround

---
+++ Yotunn/Assets/Scripts/GameEngine/Command.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Command {
+
+    //Une queue static pour que toute les objets qui hérite de command partage la queue
+    public static Queue<Command> CommandQueue = new Queue<Command>();
+    //Une booleen qui fait la fonction lock si une command est en procédure, laquelle est aussi partagé avec toutes les objet qui hérite de commande
+    public static bool CommandIsExecuting;
+
+    public delegate void CommandToExecute();
+
+    //public Command(Func<> pCommandToExecute)
+    //{
+
+    //}
+
+    //Ajout une commande à la queue
+    public void AppendCommandToQueue()
+    {
+        //Ajoute la commande à la queue
+        CommandQueue.Enqueue(this);
+    }
+    
+    
+}
+
+
